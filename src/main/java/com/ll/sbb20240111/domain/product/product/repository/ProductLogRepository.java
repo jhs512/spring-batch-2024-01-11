a99d08dd9b49af71047daa1ef404a49d2b2c848f
jhs512 <jangka512@gmail.com>
2024-01-16 11:09:19 +0900

    work

---
+++ src/main/java/com/ll/sbb20240111/domain/product/product/repository/ProductLogRepository.java
@@ -0,0 +1,7 @@
+package com.ll.sbb20240111.domain.product.product.repository;
+
+import com.ll.sbb20240111.domain.product.product.entity.ProductLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductLogRepository extends JpaRepository<ProductLog, Long> {
+}
