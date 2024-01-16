b22ad72e2bfa97c5d20173450902e8f818026592
jhs512 <jangka512@gmail.com>
2024-01-16 10:17:10 +0900

    work

--- src/main/java/com/ll/sbb20240111/batch/Hello4JobConfig.java
+++ src/main/java/com/ll/sbb20240111/batch/Hello4JobConfig.java
@@ -1,5 +1,6 @@
 package com.ll.sbb20240111.batch;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobScope;
@@ -8,7 +9,6 @@ import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
@@ -31,6 +31,16 @@ public class Hello4JobConfig {
     }
 
     @JobScope
+    @Component
+    public static class Hello4Step1Counter {
+        private int count = 0;
+
+        public void printCount(String where) {
+            System.out.println(" count = " + ++count + " in " + where);
+        }
+    }
+
+    @JobScope
     @Bean
     public Step hello4Step1(
             JobRepository jobRepository,
@@ -50,10 +60,14 @@ public class Hello4JobConfig {
     // 원본 데이터 읽기
     @StepScope
     @Component
+    @RequiredArgsConstructor
     public static class Hello4Step1Reader implements ItemReader<Integer> {
+        private final Hello4Step1Counter hello4Step1Counter;
+
         @Override
         public Integer read() {
-            int no = (int) (Math.random() * 500);
+            hello4Step1Counter.printCount("Reader");
+            int no = (int) (Math.random() * 200);
 
             if (no == 100) return null;
 
@@ -65,9 +79,14 @@ public class Hello4JobConfig {
     // EX : 50 -> "no. 50"
     @StepScope
     @Component
+    @RequiredArgsConstructor
     public static class Hello4Step1Processor implements ItemProcessor<Integer, String> {
+        private final Hello4Step1Counter hello4Step1Counter;
+
         @Override
         public String process(Integer item) {
+            hello4Step1Counter.printCount("Processor");
+
             return "no. " + item;
         }
     }
@@ -75,13 +94,15 @@ public class Hello4JobConfig {
     // 파생 데이터를 화면에 출력
     @StepScope
     @Component
+    @RequiredArgsConstructor
     public static class Hello4Step1Writer implements ItemWriter<String> {
+        private final Hello4Step1Counter hello4Step1Counter;
 
         @Override
         public void write(Chunk<? extends String> chunk) {
             List<String> items = (List<String>) chunk.getItems();
             for (String item : items) {
-                System.out.println("item = " + item);
+                hello4Step1Counter.printCount("Writer, item = " + item);
             }
         }
     }
--- src/test/java/com/ll/sbb20240111/batch/BatchTestConfig.java
+++ src/test/java/com/ll/sbb20240111/batch/BatchTestConfig.java
@@ -34,4 +34,11 @@ public class BatchTestConfig {
         utils.setJob(hello4Job);
         return utils;
     }
+
+    @Bean
+    public JobLauncherTestUtils hello5JobLauncherTestUtils(Job hello5Job) {
+        JobLauncherTestUtils utils = new JobLauncherTestUtils();
+        utils.setJob(hello5Job);
+        return utils;
+    }
 }
